f2319ac239ae539910dfa1c35e5e56d2fb0f9fe0
Angelina S <Angelinko.lol@gmail.com>
2019-08-06 23:54:11 +0300

    Update Variable.java
    Добавлена в 11 строку пустая строка.
    Строки кода сдвинуты, для лучшего восприятия.

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,20 +1,21 @@
 public class Variable {
 	public static void main(String[] args) {
-byte core = 2;
-short threads = 4;
-int memory = 500;
-long cost = 1_500_000L;
-float hddMemory = 4_500_000f;
-double frequency = 1.5;
-char name = 'a';
-boolean isWork = true;
-System.out.println(core);
-System.out.println(threads);
-System.out.println(memory);
-System.out.println(cost);
-System.out.println(hddMemory);
-System.out.println(frequency);
-System.out.println(name);
-System.out.println(isWork);
+		byte core = 2;
+		short threads = 4;
+		int memory = 500;
+		long cost = 1_500_000L;
+		float hddMemory = 4_500_000f;
+		double frequency = 1.5;
+		char name = 'a';
+		boolean isWork = true;
+		
+		System.out.println(core);
+		System.out.println(threads);
+		System.out.println(memory);
+		System.out.println(cost);
+		System.out.println(hddMemory);
+		System.out.println(frequency);
+		System.out.println(name);
+		System.out.println(isWork);
 	}
 }
