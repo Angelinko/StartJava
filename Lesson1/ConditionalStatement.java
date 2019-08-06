e61426e45058a2cf1ea731d23166bd24867da750
Angelina S <Angelinko.lol@gmail.com>
2019-08-07 00:06:45 +0300

    Update ConditionalStatement.java
    Изменено имя переменных, так же убрано сравнение в if.  Убрана булева переменная.

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -5,33 +5,29 @@ public class ConditionalStatement {
 			System.out.println("Age more 20");
 		}
 		
-		boolean isSexMale = false;
-		if (isSexMale == true) {
+		boolean isMale = false;
+		if (isMale) {
 			System.out.println("You man");
 		}
 		
-		boolean isSexFemale = true;
-		if (isSexFemale == true) {
+		boolean isFemale = true;
+		if (isFemale) {
 			System.out.println("You woman");
 		}
 		
 		double height = 1.72;
 		if (height < 1.80) {
 			System.out.println("height < 1.80");
-		}
-		else {
+		} else {
 			System.out.println("height > 1.80");
 		}
 
-		String name = "Angelina";
-		boolean yourNameStartsOn;
-		if (yourNameStartsOn = name.startsWith("M")) {
-			System.out.println("Name starts at M - " + yourNameStartsOn);
-		}
-		else if (yourNameStartsOn = name.startsWith("I")) {
-			System.out.println("Name starts at I - " + yourNameStartsOn);
-		}
-		else {
+		 char firstLetterName = 'A';
+		if (firstLetterName == 'M') {
+			System.out.println("Name starts at M");
+		} else if (firstLetterName == 'I') {
+			System.out.println("Name starts at I");
+		} else {
 			System.out.println("Name starts at another letters");
 		}
 	}
