1dec85abf0893c568b47869e5852559c65b1a871
Angelina S <Angelinko.lol@gmail.com>
2019-08-08 16:29:19 +0300

    Update Cycle.java

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,5 +1,5 @@
 public class Cycle {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		for(int i = 0; i <= 20; i++) {
 			System.out.println(i);
 		}
@@ -12,9 +12,9 @@ public class Cycle {
 
 		counter = 10;
 		int sumOdd = 0;
-		do{
-			if (counter % 2 != 0){
-				sumOdd = sumOdd + counter;
+		do {
+			if (counter % 2 != 0) {
+				sumOdd += counter;
 			}
 			counter++;
 		} while(counter != 20);
