4b40e3a2029615e2f3a7a7e9f14c4ad11631ac7a
Angelina S <Angelinko.lol@gmail.com>
2019-08-10 02:27:56 +0300

    Create MyFirstGame.java
    Выполнено 5е ДЗ.

---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,17 @@
+public class MyFirstGame {
+	public static void main(String[] args) {
+		int guessNumber = 61;
+		int trialNumber = 89;
+
+		while (trialNumber != guessNumber) {
+			if (trialNumber > guessNumber) {
+				System.out.println("Your number is greate");
+			trialNumber--;
+			} else {
+				System.out.println("Your number is less");
+				trialNumber++;
+			}
+			System.out.println(trialNumber);
+		}
+	}
+}
