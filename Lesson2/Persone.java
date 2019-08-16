b477d186530f1e0b5308659a0a640c530182a7aa
Angelina S <Angelinko.lol@gmail.com>
2019-08-16 16:38:16 +0300

    Create Persone.java

---
+++ Lesson2/Persone.java
@@ -0,0 +1,28 @@
+public class Persone {
+
+	String sex = "woman";
+	String name = "Angelina";
+	double height = 1.72;
+	double weight = 62;
+	int age = 20;
+
+	void go() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void speak() {
+
+	}
+
+	void learnJava() {
+		
+	}
+}
