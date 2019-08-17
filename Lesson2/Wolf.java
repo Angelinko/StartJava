cf9dcfdf7a2deaeb2b9fead571fe680826345549
Angelina S <Angelinko.lol@gmail.com>
2019-08-17 16:13:23 +0300

    Create Wolf.java

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+	
+	String sex = "man";
+	String nickname = "Kai";
+	double weight = 52;
+	int age = 3;
+	String color = "black";
+
+	void go() {
+		System.out.println("Wolf go");
+	}
+
+	void sit() {
+		System.out.println("Wolf sit");
+	}
+
+	void run() {
+		System.out.println("Wolf run");
+	}
+
+	void howl() {
+		System.out.println("Wolf howl");
+	}
+
+	void hunt() {
+		System.out.println("Wolf hunt");		
+	}
+}
