f4a94c2edcfba06374be9dd83a0608ad074e9ed0
Angelina S <Angelinko.lol@gmail.com>
2019-08-17 16:13:44 +0300

    Create WolfTest.java

---
+++ Lesson2/WolfTest.java
@@ -0,0 +1,17 @@
+public class WolfTest {
+	public static void main(String[] args) {
+
+		Wolf wolfOne = new Wolf();
+		System.out.println("Sex = " + wolfOne.sex);
+		System.out.println("Nickname = " + wolfOne.nickname);
+		System.out.println("Weight = " + wolfOne.weight);
+		System.out.println("Age = " + wolfOne.age);
+		System.out.println("Color = " + wolfOne.color);
+
+		wolfOne.go();
+		wolfOne.sit();
+		wolfOne.run();
+		wolfOne.howl();
+		wolfOne.hunt();
+	}
+}
