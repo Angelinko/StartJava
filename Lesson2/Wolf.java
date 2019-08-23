9460ff237e82b710eaedf4dfa6518ad674d37ff3
Angelina S <Angelinko.lol@gmail.com>
2019-08-23 15:20:31 +0300

    Update Wolf.java
    Добавлены геттеры и сеттеры

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
 	
-	String sex = "man";
-	String nickname = "Kai";
-	double weight = 52;
-	int age = 3;
-	String color = "black";
+	private String sex = "man";
+	private String nickname = "Kai";
+	private double weight = 52;
+	private int age = 3;
+	private String color = "black";
 
-	void go() {
+	public String getSex() {
+		return sex;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	void setAge(int age) {
+		if (age > 8) {
+			System.out.println("incorrect age");
+		} else {
+			this.age = age;
+		}
+	}
+
+	void setColor(String color) {
+		this.color = color;
+	}
+
+	public void go() {
 		System.out.println("Wolf go");
 	}
 
-	void sit() {
+	public void sit() {
 		System.out.println("Wolf sit");
 	}
 
-	void run() {
+	public void run() {
 		System.out.println("Wolf run");
 	}
 
-	void howl() {
+	public void howl() {
 		System.out.println("Wolf howl");
 	}
 
-	void hunt() {
+	public void hunt() {
 		System.out.println("Wolf hunt");		
 	}
 }
