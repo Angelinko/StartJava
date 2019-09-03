1cf41d863ccf83ca26e674dbb8cad8e85cfe0dd9
Angelina S <Angelinko.lol@gmail.com>
2019-09-04 00:36:41 +0300

    Create Calculator.java

---
+++ Lesson2/Calculator.java
@@ -0,0 +1,56 @@
+public class Calculator {
+	
+		private int firstNumber;
+		private int secondNumber;
+		private int result;
+		private String sign;
+
+		public int getFirstNumber() {
+		return firstNumber;
+	}
+
+	public int getSecondNumber() {
+		return secondNumber;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+	public String getSign() {
+		return sign;
+	}
+
+	public void setFirstNumber(int firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public void setSecondNumber(int secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+
+	public void setSign(String sign) {
+		this.sign = sign;
+	}
+
+	public void setResult() {
+		if (sign.equals("*")) {
+			result = firstNumber * secondNumber;
+		} else if (sign.equals("/")) {
+			result = firstNumber / secondNumber;
+		} else if (sign.equals("+")) {
+			result = firstNumber + secondNumber;
+		} else if (sign.equals("-")) {
+			result = firstNumber - secondNumber;
+		} else if (sign.equals("%")) {
+			result = firstNumber % secondNumber;
+		} else if (sign == ("^")) {
+			result = 1;
+			for (int i = 0; i < secondNumber; i++) {
+				result *= firstNumber;
+			}
+		} else {
+			System.out.println("Error");
+		}
+	}
+}
