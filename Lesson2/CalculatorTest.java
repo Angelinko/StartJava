afe08bbcaf56f7bf2f25290bf59e10d1eda443ff
Angelina S <Angelinko.lol@gmail.com>
2019-09-04 00:36:51 +0300

    Create CalculatorTest.java

---
+++ Lesson2/CalculatorTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+	public static void main(String[] args) {
+		Calculator calculatorOne = new Calculator();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		int number1 = sc.nextInt();
+		calculatorOne.setFirstNumber(number1);
+		System.out.println("Enter the second number:");
+		int number2 = sc.nextInt();
+		calculatorOne.setSecondNumber(number2);
+		System.out.println("Enter character:");
+		String sign = sc.next();
+		calculatorOne.setSign(sign);
+		calculatorOne.setResult();
+		System.out.println("Result " + calculatorOne.getResult());
+	}
+}
