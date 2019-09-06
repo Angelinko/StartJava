98656ddd95edf25581752ab2300800700b01dc29
Angelina S <Angelinko.lol@gmail.com>
2019-09-06 14:22:19 +0300

    Update CalculatorTest.java

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
-		boolean isAlive = true;
-		while (isAlive) {
+		Scanner sc = new Scanner(System.in);
+		String answer;
+
+		do {
 			Calculator calculatorOne = new Calculator();
-			Scanner sc = new Scanner(System.in);
+
 			System.out.println("Enter the first number:");
 			int number1 = sc.nextInt();
 			calculatorOne.setFirstNumber(number1);
+
 			System.out.println("Enter the second number:");
 			int number2 = sc.nextInt();
 			calculatorOne.setSecondNumber(number2);
+
 			System.out.println("Enter character:");
 			String sign = sc.next();
 			calculatorOne.setSign(sign);
-			calculatorOne.result();
-			boolean isContinue = true;
-			while (isContinue) {
-				System.out.println("Continue? (yes or no");
-				String answer = sc.next();
-				if (answer.equalsIgnoreCase("no")) {
-					isAlive = false;
-					isContinue = false;
-				} else if (answer.equalsIgnoreCase("yes")) {
-					isContinue = false;
-				}
-			}
-		}
+
+			calculatorOne.calculate();
+			
+			do {
+				System.out.println("Continue? (yes or no)");
+				answer = sc.next();
+			} while (!answer.equalsIgnoreCase("no") && !answer.equalsIgnoreCase("yes"));
+		} while (answer.equalsIgnoreCase("yes"));
 	}
 }
