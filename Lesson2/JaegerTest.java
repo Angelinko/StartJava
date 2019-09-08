e519862808f2395ec50695c55b8e9af8a996ab4e
Angelina S <Angelinko.lol@gmail.com>
2019-09-08 16:45:26 +0300

    Update JaegerTest.java

--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -1,24 +1,24 @@
 public class JaegerTest {
 	public static void main(String[] args) {
-		Jaeger gaegerOne = new Jaeger();
-		gaegerOne.setModelName("Romeo Blue");
-		gaegerOne.setMark("Mark-1");
-		gaegerOne.setOrigin("USA");
-		gaegerOne.setHeight(77.72);
-		gaegerOne.setWeight(7.775);
-		gaegerOne.setSpeed(2);
-		gaegerOne.setStrength(7);
-		gaegerOne.setArmor(6);
+		Jaeger gaegerOne = new Jaeger("Romeo Blue", "Mark-1", "USA", 77.72, 7.775, 2, 7, 6);
+		// gaegerOne.setModelName("Romeo Blue");
+		// gaegerOne.setMark("Mark-1");
+		// gaegerOne.setOrigin("USA");
+		// gaegerOne.setHeight(77.72);
+		// gaegerOne.setWeight(7.775);
+		// gaegerOne.setSpeed(2);
+		// gaegerOne.setStrength(7);
+		// gaegerOne.setArmor(6);
 
-		Jaeger gaegerTwo = new Jaeger();
-		gaegerTwo.setModelName("Crimson Typhoon");
-		gaegerTwo.setMark("Mark-4");
-		gaegerTwo.setOrigin("China");
-		gaegerTwo.setHeight(76.2);
-		gaegerTwo.setWeight(1.722);
-		gaegerTwo.setSpeed(9);
-		gaegerTwo.setStrength(8);
-		gaegerTwo.setArmor(6);
+		Jaeger gaegerTwo = new Jaeger("Crimson Typhoon", "Mark-4", "China", 76.2, 1.722, 9, 8, 6);
+		// gaegerTwo.setModelName("Crimson Typhoon");
+		// gaegerTwo.setMark("Mark-4");
+		// gaegerTwo.setOrigin("China");
+		// gaegerTwo.setHeight(76.2);
+		// gaegerTwo.setWeight(1.722);
+		// gaegerTwo.setSpeed(9);
+		// gaegerTwo.setStrength(8);
+		// gaegerTwo.setArmor(6);
 
 		System.out.println("ModelName = " + gaegerOne.getModelName());
 		System.out.println("Mark = " + gaegerOne.getMark());
