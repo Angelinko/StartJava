ec8d712688339a832d6e33e1edffb26acc0212fc
Angelina S <Angelinko.lol@gmail.com>
2019-09-17 16:28:20 +0300

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
+
 public class GuessNumber {
 
-	static boolean isGuess = true;
-	static int random = (int)(Math.random() * 100);
-	static Scanner sc = new Scanner(System.in);
+	Player playerOne;
+	Player playerTwo;
+	public boolean isGuess = true;
+	public int random = (int)(Math.random() * 101);
+	public Scanner sc = new Scanner(System.in);
 
-	public static void number(String name1,String name2) {
+	public void startGame(String name1, String name2) {
 		while (isGuess) {
 			System.out.println("First player guess the number:");
 			int number1 = sc.nextInt();
-			Player playerOne = new Player(name1, number1);
+			playerOne = new Player(name1, number1);
 
 			System.out.println("Second player guess the number:");
 			int number2 = sc.nextInt();
-			Player playerTwo = new Player(name2, number2);
+			playerTwo = new Player(name2, number2);
 
-			findNumber(playerOne);
-			findNumber(playerTwo);
+			compareNumbers(playerOne);
+			compareNumbers(playerTwo);
 		}
 	}
 
-	public static void findNumber (Player player) {
+	public void compareNumbers(Player player) {
 		int playerNumber = player.getNumber();
 		if (playerNumber > random) {
 			System.out.println(player.getName() + " - Your number is greate");
