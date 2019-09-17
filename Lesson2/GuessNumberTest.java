73f8193869d1f0ee9cbb3eb762340af6764b4671
Angelina S <Angelinko.lol@gmail.com>
2019-09-17 16:59:19 +0300

    Update GuessNumberTest.java

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
 	public static void main(String[] args) {
+		GuessNumber guessNumber = new GuessNumber();
 		Scanner sc = new Scanner(System.in);
 		String answer;
 
-		System.out.println("Enter the name of the first player:");
-			String name1 = sc.next();
-		
-			System.out.println("Enter the name of the second player:");
-			String name2 = sc.next();
 		do {
-			GuessNumber guessNumber = new GuessNumber();
-			guessNumber.startGame(name1, name2);
-			// GuessNumber.startGame(name1, name2);
+			guessNumber.startGame();
 			do {
 				System.out.println("Continue? (yes or no)");
 				 answer = sc.next();
