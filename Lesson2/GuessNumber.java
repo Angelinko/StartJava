173fbcd8df256dedc52e36ed92b40a394230cbed
Angelina S <Angelinko.lol@gmail.com>
2019-09-18 01:46:00 +0300

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -2,42 +2,41 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-	Player playerOne;
-	Player playerTwo;
-	public boolean isGuess = true;
+	private Player playerOne;
+	private Player playerTwo;
 	public int random = (int)(Math.random() * 101);
 	public Scanner sc = new Scanner(System.in);
 
-	public void startGame() {
-		System.out.println("Enter the name of the first player:");
-		String name1 = sc.next();
-		
-		System.out.println("Enter the name of the second player:");
-		String name2 = sc.next();
-		
-		while (isGuess) {
-			System.out.println("First player guess the number:");
-			int number1 = sc.nextInt();
-			playerOne = new Player(name1, number1);
-
-			System.out.println("Second player guess the number:");
-			int number2 = sc.nextInt();
-			playerTwo = new Player(name2, number2);
-
-			compareNumbers(playerOne);
-			compareNumbers(playerTwo);
-		}
+	public GuessNumber(Player playerOne, Player playerTwo){
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
 	}
 
-	public void compareNumbers(Player player) {
-		int playerNumber = player.getNumber();
-		if (playerNumber > random) {
-			System.out.println(player.getName() + " - Your number is greate");
-		} else if (playerNumber < random) {
-			System.out.println(player.getName() + " - Your number is less");	
-		} else {
-			System.out.println(player.getName() + " winner!");
-			isGuess = false;
-		}
-	}	
+	public void startGame() {
+		do{
+			System.out.println("First player guess the number:");
+			playerOne.setNumber(sc.nextInt());
+
+			int playerNumberOne = playerOne.getNumber();
+			if (playerNumberOne > random) {
+				System.out.println(playerOne.getName() + " - Your number is greate");
+			} else if (playerNumberOne < random) {
+				System.out.println(playerOne.getName() + " - Your number is less");	
+			} else {
+				System.out.println(playerOne.getName() + " winner!");
+			}
+
+			System.out.println("Second player guess the number:");
+			playerTwo.setNumber(sc.nextInt());
+
+			int playerNumberTwo = playerTwo.getNumber();
+			if (playerNumberTwo > random) {
+				System.out.println(playerTwo.getName() + " - Your number is greate");
+			} else if (playerNumberTwo < random) {
+				System.out.println(playerTwo.getName() + " - Your number is less");	
+			} else {
+				System.out.println(playerTwo.getName() + " winner!");
+			}
+		}while(playerOne.getNumber() != random && playerTwo.getNumber() != random);
+	}
 }
